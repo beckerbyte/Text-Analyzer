34ee4cab202da935943df01eb1a74371e9d985cd
beckerbyte <julian.becker123@gmail.com>
2025-07-16 23:15:38 +0200

    Delete analyzerold.py

--- analyzerold.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import scrolledtext, filedialog, messagebox, Toplevel, PhotoImage, ttk
-from collections import Counter
-import re
-import os
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
